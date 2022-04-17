4db7ef7a32fd62f84bcf41593f382bdc02992c21
Joaquin Re <joaquinre123@gmail.com>
2022-04-17 12:05:03 -0300

    set engine template

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express')
-const connectDB = require('./app/config/db')
+// const connectDB = require('./app/config/db')
+
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
 
 const app = express()
 
 // Connect Database
-connectDB();
+// connectDB();
 
-app.get('/', (req, res) => res.send('hello'))
+app.get('/', (req, res) => {
+	res.render('home')
+})
+
+// set Template engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
 
 const port = process.env.PORT || 8082
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => 
+	console.log(`Server running on port ${port}`));
